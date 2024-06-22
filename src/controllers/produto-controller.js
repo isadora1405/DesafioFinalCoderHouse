a6acfa259f758e8cc5540e7fb16c0ae5ecc44ed3
Carlos Eduardo Souza de Oliveira <edubahia.carlos@hotmail.com>
2024-06-22 15:59:55 -0300

    Adicao da regra de limit

--- src/controllers/produto-controller.js
+++ src/controllers/produto-controller.js
@@ -1,12 +1,18 @@
 const produtos = [{id: 1, nome: "Teste"}];
 const ProductManager = require('../services/product-manager');
-const produtoManager = new ProductManager();
+const productManager = new ProductManager();
 
-const getProdutos = ('/', (req, res) => {
-    produtoManager.getProduct().then(dados => {
-        res.send(dados);
-    })
-    
+const getProdutos = ('/', (request, response) => {
+    const limit = request.query.limit;
+    productManager.getProduct().then(res => {
+        if (limit && res.length && res.length > limit) {
+            response.send(res.slice(0, limit));
+            return;
+        }
+            response.send(res);
+        }).catch(erro => {
+            response.send("errro: ", erro);
+        })    
 });
 ;
 module.exports = {
